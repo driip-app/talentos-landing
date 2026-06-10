fd7e885e458ac95b15af614c551694a78381cdab
Munir Uddin Rohan <muniruddinrohan@gmail.com>
2026-06-10 20:57:00 +0600

    Adds logo and fixes hero section in mobile

--- src/comps/landing/Nav.tsx
+++ src/comps/landing/Nav.tsx
@@ -26,7 +26,7 @@ export default function Nav({ onOpenWaitlist }: NavProps) {
       <nav style={isMobile ? { justifyContent: "center" } : undefined}>
         <a href="#top" className="nav-logo">
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/images/driip-logo.png" alt="Driip" />
+          <img src="/images/driip-logo.png" alt="Driip" style={{ mixBlendMode: "screen" }} />
         </a>
         {!isMobile && (
           <div className="nav-links">
