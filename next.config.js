df74206c9f6e81c1961a204feb797ef24e8f8e28
Munir Uddin Rohan <muniruddinrohan@gmail.com>
2026-07-14 22:14:19 +0600

    adds deck redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/deck',
+        destination: 'https://canva.link/1lw4pxds6m7jrbr',
+        permanent: false,
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
